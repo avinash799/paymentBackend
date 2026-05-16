cef88285a34035c0cc2acf023ab83228d708f873
AVINASH RAI <76801210+avinash799@users.noreply.github.com>
2026-05-16 14:28:15 +0530

    some changes in redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,30 @@
+
+
 import Redis from "ioredis";
 
-const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
+const redis = new Redis(
+  process.env.REDIS_URL,
+  {
+    retryStrategy: (times) => {
+      return Math.min(
+        times * 50,
+        2000
+      );
+    },
+  }
+);
+
+redis.on("connect", () => {
+  console.log(
+    "Redis Connected"
+  );
+});
+
+redis.on("error", (err) => {
+  console.log(
+    "Redis Error:",
+    err.message
+  );
 });
 
 export default redis;
